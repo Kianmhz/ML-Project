72be5dbbc29ed651e26ad8303f2fe871de7a5319
Kian Haddad <haddad.kianm@gmail.com>
2026-02-18 23:49:24 -0500

    remove unused files and imports

--- models/logistic_regression_model.py
+++ models/logistic_regression_model.py
@@ -4,7 +4,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import numpy as np
-import json
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, roc_auc_score, confusion_matrix
 
 # Importing Dataset
